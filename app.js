54e6ec1139d1ed3c343c5acac8dd4d11e2b6a052
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-02-28 15:52:28 +0530

    Server is created and html added

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
+
 const server = http.createServer((req,res)=>{
-    res.end('Abhimanyu Singh');
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head>')
+    res.write('<title>My first Page</title>')
+    res.write('</head>')
+    res.write('<body>')
+    res.write('<h1>Welcome to my nodejs project</h1>')
+    res.write('</body>')
+    res.write('</html>')
+    res.end();
 })
-server.listen(4000,()=>{
-    console.log("Hello World");
-});
+
+server.listen(4000);
