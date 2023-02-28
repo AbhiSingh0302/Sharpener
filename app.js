b676cd3d94a3831d5e423e15ddd66a11589c455e
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-02-28 15:57:11 +0530

    different response on different url endpt hit

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const server = http.createServer((req,res)=>{
     res.write('<title>My first Page</title>')
     res.write('</head>')
     res.write('<body>')
-    res.write('<h1>Welcome to my nodejs project</h1>')
+    if(req.url == "/home"){
+        res.write("<h1>Welcome Home</h1>");
+    }else if(req.url == "/about"){
+        res.write("<h1>Welcome to about section</h1>");
+    }else if(req.url == "/node"){
+        res.write("<h1>Welcome to my Node Js project</h1>");
+    }else{
+        res.write('<h1>Welcome to my nodejs project</h1>')
+    }
     res.write('</body>')
     res.write('</html>')
     res.end();
