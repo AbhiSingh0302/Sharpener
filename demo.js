9fe00f89cdaa306d2e9cac75922457a34c4634d6
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-03-02 12:43:37 +0530

    Chat app demo

--- demo.js
+++ demo.js
@@ -1,1 +1,44 @@
-console.log("welcome");
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        return fs.readFile('message.txt','utf-8',(err,data)=>{
+            if(err){
+                console.log(err);
+                return;
+            }
+            res.write('<html>')
+            res.write('<head><title>Enter Message</title></head>')
+            res.write(`<body><p>${data}</p><form action="/message" method="POST"><input type="text" name="message"><button>Send</button></form></body>`)
+            res.write('</html>')
+            return res.end();
+        })
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk)=>{
+            // console.log(chunk);
+            body.push(chunk);
+        })
+        return req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            // console.log(message);
+            // fs.writeFileSync('message.txt',message);
+            fs.writeFile('message.txt',message,err=>{
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            })
+        })
+    }
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><h1>Hello from my node.js server!</h1></body>')
+    res.write('</html>')
+})
+server.listen(3000);
